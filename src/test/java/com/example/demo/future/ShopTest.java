c7e01d2dbf89980dea07fbbb54492fc755d35e60
choss01 <choss01@oneplat.co>
2022-04-19 18:00:37 +0900

    비동기api가 에러 났을때 처리가 안되어있는 상황

--- src/test/java/com/example/demo/future/ShopTest.java
+++ src/test/java/com/example/demo/future/ShopTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.Future;
 
+import static com.example.demo.future.DelayUtil.delay;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShopTest {
@@ -15,6 +16,20 @@ class ShopTest {
     Future<Double> futurePrice = shop.getPriceAsync("my favorite product");
     long invocationTime = ((System.nanoTime() - start) / 1_000_000);
     System.out.println("Invocation returned after " + invocationTime + " msecs ");
+    doSomethingElse();
+    try{
+      double price = futurePrice.get();
+      System.out.printf("Price is %.2f%n", price);
+    }catch(Exception e){
+      throw new RuntimeException(e);
+    }
+    long retrievalTime = ((System.nanoTime() - start) / 1_000_000);
+    System.out.println("Price returned after " + retrievalTime + " msecs");
+
+  }
+
+  private void doSomethingElse(){
+    delay();
   }
 
 }
