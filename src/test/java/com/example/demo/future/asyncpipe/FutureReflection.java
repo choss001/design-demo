34cc88e1088f263a3d4f452fdb677d0ea6da7ecf
jss <choss01@oneplat.co>
2022-04-21 21:02:09 +0900

    Future Reflection

---
+++ src/test/java/com/example/demo/future/asyncpipe/FutureReflection.java
@@ -0,0 +1,42 @@
+package com.example.demo.future.asyncpipe;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FutureReflection {
+
+  private Shop shop = new Shop("product");
+  ExecutorService executor = Executors.newCachedThreadPool();
+  final Future<Double> futureRate = executor.submit(new Callable<Double>(){
+    public Double call() {
+      return exchangeService.getRate(Money.EUR, Money.USD);
+    }
+  });
+
+  Future<Double> futurePriceInUSD = executor.submit(new Callable<Double>() {
+    @Override
+    public Double call() throws Exception {
+      double priceInEUR = shop.getPrice("product");
+      return priceInEUR * futureRate.get();
+      return null;
+    }
+  });
+
+  public enum Money{
+    EUR(5), USD(10);
+    private final Integer percent;
+    Money(Integer percent){
+      this.percent = percent;
+    }
+  }
+
+
+  @Test
+  void futureReflection(){
+
+  }
+}
