3e7f2a52cbed7a83fd597343bc8f169f2677f285
jss <choss01@oneplat.co>
2022-04-22 20:21:21 +0900

    random delay로 수정

--- src/main/java/com/example/demo/future/DelayUtil.java
+++ src/main/java/com/example/demo/future/DelayUtil.java
@@ -1,10 +1,22 @@
 package com.example.demo.future;
 
+import java.util.Random;
+
 public class DelayUtil {
   public static void delay(){
     try{
       Thread.sleep(1000L);
     }catch(InterruptedException e){
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static final Random random = new Random();
+  public static void randomDelay() {
+    int delay = 500 + random.nextInt(2000);
+    try {
+      Thread.sleep(delay);
+    } catch (InterruptedException e) {
       throw new RuntimeException(e);
     }
   }
--- src/test/java/com/example/demo/future/asyncpipe/FutureReflection.java
+++ src/test/java/com/example/demo/future/asyncpipe/FutureReflection.java
@@ -12,18 +12,19 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
 
 public class FutureReflection {
   private static final Double DEFAULT_RATE = 10d;
 
   final List<Shop> shops = Arrays.asList(new Shop("BestPrice"),
-    new Shop("MyFavoriteShop"),
-    new Shop("BuyItAll"));
+      new Shop("MyFavoriteShop"),
+      new Shop("BuyItAll"));
 
   private Shop shop = new Shop("product");
   ExecutorService executor = Executors.newCachedThreadPool();
-  final Future<Double> futureRate = executor.submit(new Callable<Double>(){
+  final Future<Double> futureRate = executor.submit(new Callable<Double>() {
     public Double call() {
       ExchangeService exchangeService = new ExchangeService();
       return exchangeService.getRate(Money.EUR, Money.USD);
@@ -31,52 +32,59 @@ public class FutureReflection {
   });
 
   Future<Double> futurePriceInUSD = CompletableFuture.supplyAsync(() -> shop.getPriceDouble("product"))
-    .thenCombine(
-      CompletableFuture.supplyAsync(
-        () -> new ExchangeService().getRate(Money.EUR, Money.USD))
-        .completeOnTimeout(DEFAULT_RATE, 1, TimeUnit.SECONDS),
-      (price, rate) -> price * rate
-    )
-    .orTimeout(3, TimeUnit.SECONDS);
+      .thenCombine(
+          CompletableFuture.supplyAsync(
+                  () -> new ExchangeService().getRate(Money.EUR, Money.USD))
+              .completeOnTimeout(DEFAULT_RATE, 1, TimeUnit.SECONDS),
+          (price, rate) -> price * rate
+      )
+      .orTimeout(3, TimeUnit.SECONDS);
 
-  public enum Money{
+  public enum Money {
     EUR(5), USD(10);
     private final Integer percent;
-    Money(Integer percent){
+
+    Money(Integer percent) {
       this.percent = percent;
     }
   }
 
   public Stream<CompletableFuture<String>> findPricesStream(String product) {
     return shops.stream()
-      .map(shop -> CompletableFuture.supplyAsync(
-        () -> shop.getPriceDouble(product), executor))
-      .map(future -> future.thenApply(Quote::doubleFromParse))
-      .map(future -> future.thenCompose(quote -> CompletableFuture.supplyAsync(
-        () -> Discount.applyDiscount(quote), executor)))
+        .map(shop -> CompletableFuture.supplyAsync(
+            () -> shop.getPrice(product), executor))
+        .map(future -> future.thenApply(Quote::parse))
+        .map(future -> future.thenCompose(quote -> CompletableFuture.supplyAsync(
+            () -> Discount.applyDiscount(quote), executor)));
   }
 
 
   @Test
-  void futureReflection(){
-    CompletableFuture[] futures = findPricesStream("myPhone")
-        .map(f -> f.thenAccept(System.out::println))
+  void futureReflection() {
+    long start = System.nanoTime();
+    CompletableFuture[] futures = findPricesStream("myPhone275")
+        .map(f -> f.thenAccept(
+            s -> System.out.println(s + " (done in " +
+                ((System.nanoTime() - start) / 1_000_000) + " msecs)")))
         .toArray(size -> new CompletableFuture[size]);
     CompletableFuture.allOf(futures).join();
-
+    System.out.println("All shops have now responded in " +
+        ((System.nanoTime() - start) / 1_000_000 + " msecs"));
   }
-  static class ExchangeService{
-    public Double getRate(Money eur, Money usd){
+
+  static class ExchangeService {
+    public Double getRate(Money eur, Money usd) {
       return 15d;
     }
   }
 
   private static final Random random = new Random();
-  public static void randomDelay(){
+
+  public static void randomDelay() {
     int delay = 500 + random.nextInt(2000);
-    try{
+    try {
       sleep(delay);
-    }catch(InterruptedException e ){
+    } catch (InterruptedException e) {
       throw new RuntimeException(e);
     }
   }
