efdb42d2b254ee7c9014bd7df6b5321e8b4f8c2d
chloe97 <chloe@oneplat.co>
2022-04-25 08:39:59 +0900

    회사가기전

---
+++ src/test/java/com/example/demo/compleatbleFuture/ShopTest.java
@@ -0,0 +1,34 @@
+package com.example.demo.compleatbleFuture;
+
+import java.util.concurrent.Future;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class ShopTest {
+
+  @Test
+  void test() {
+    Shop shop = new Shop("BestShop");
+    long start = System.nanoTime();
+    Future<Double> futurePrice = shop.getPriceAsync("my favorite product");
+    long invocationTime = ((System.nanoTime() - start) / 1_000_000);
+    log.info("Invocation returned after {} msecs", invocationTime);
+    doSomethingElse();
+    log.info("somethingElse end !!!");
+    try {
+      double price = futurePrice.get();
+      log.info("price is {} ", price);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    long retrievalTime = ((System.nanoTime() - start) / 1_000_000);
+    log.info("Price returned after {} mescs", retrievalTime);
+  }
+
+  private void doSomethingElse() {
+    log.info("doSomethingElse !!!");
+//    Shop.deleay();
+  }
+
+}
