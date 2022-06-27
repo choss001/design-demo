355983a6623147e58a382a88942e43218c5aa364
choss001 <choss01@naver.com>
2022-06-27 23:45:24 +0900

    상속보다는 합성을 사용하라

---
+++ src/main/java/com/example/demo/object/_01/_08/Call.java
@@ -0,0 +1,4 @@
+package com.example.demo.object._01._08;
+
+public class Call {
+}
---
+++ src/main/java/com/example/demo/object/_01/_08/RegularPhone.java
@@ -0,0 +1,8 @@
+package com.example.demo.object._01._08;
+
+public class RegularPhone extends Phone{
+    @Override
+    protected Money calculateCallFee(Call call) {
+        return null;
+    }
+}
