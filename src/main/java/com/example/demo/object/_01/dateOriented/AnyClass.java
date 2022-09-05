5e62c8c0c242e412e81911b003ec4475fed437af
chloeakazhixian97 <84550316+chloeakazhixian97@users.noreply.github.com>
2022-09-05 23:44:26 +0900

    coding test

---
+++ src/main/java/com/example/demo/object/_01/dateOriented/AnyClass.java
@@ -0,0 +1,8 @@
+package com.example.demo.object._01.dateOriented;
+
+public class AnyClass {
+  void anyMethod(Rectangle rectangle, int multiple) {
+    rectangle.setRight(rectangle.getRight() * multiple);
+    rectangle.setBottom(rectangle.getBottom() * multiple);
+  }
+}
--- src/main/java/rxJava/DebugSubscriber.java
+++ src/main/java/rxJava/DebugSubscriber.java
@@ -1,49 +1,49 @@
-package rxJava;
-
-import io.reactivex.rxjava3.subscribers.DisposableSubscriber;
-
-public class DebugSubscriber<T> extends DisposableSubscriber<T> {
-    private String label;
-    public DebugSubscriber() {
-        super();
-    }
-
-    public DebugSubscriber(String label) {
-        super();
-        this.label = label;
-    }
-
-
-    @Override
-    public void onNext(T data) {
-        String threadName = Thread.currentThread().getName();
-        if (label == null) {
-            System.out.println(threadName + ": " + data);
-        }else {
-            System.out.println(threadName + ": " + label + ": " + data);
-        }
-    }
-
-    @Override
-    public void onError(Throwable throwable) {
-        String threadName = Thread.currentThread().getName();
-        if (label == null) {
-            System.out.println(threadName + ": ERORR! = " + throwable);
-        }else {
-            System.out.println(threadName + ": " + label + ": ERORR! = " + throwable);
-        }
-
-    }
-
-    @Override
-    public void onComplete() {
-        String threadName = Thread.currentThread().getName();
-        if (label == null) {
-            System.out.println(threadName + ": COMPLETE!");
-        }else {
-            System.out.println(threadName + ": " + label + ": COMPLETE!");
-        }
-
-    }
-}
-
+//package rxJava;
+//
+//import io.reactivex.rxjava3.subscribers.DisposableSubscriber;
+//
+//public class DebugSubscriber<T> extends DisposableSubscriber<T> {
+//    private String label;
+//    public DebugSubscriber() {
+//        super();
+//    }
+//
+//    public DebugSubscriber(String label) {
+//        super();
+//        this.label = label;
+//    }
+//
+//
+//    @Override
+//    public void onNext(T data) {
+//        String threadName = Thread.currentThread().getName();
+//        if (label == null) {
+//            System.out.println(threadName + ": " + data);
+//        }else {
+//            System.out.println(threadName + ": " + label + ": " + data);
+//        }
+//    }
+//
+//    @Override
+//    public void onError(Throwable throwable) {
+//        String threadName = Thread.currentThread().getName();
+//        if (label == null) {
+//            System.out.println(threadName + ": ERORR! = " + throwable);
+//        }else {
+//            System.out.println(threadName + ": " + label + ": ERORR! = " + throwable);
+//        }
+//
+//    }
+//
+//    @Override
+//    public void onComplete() {
+//        String threadName = Thread.currentThread().getName();
+//        if (label == null) {
+//            System.out.println(threadName + ": COMPLETE!");
+//        }else {
+//            System.out.println(threadName + ": " + label + ": COMPLETE!");
+//        }
+//
+//    }
+//}
+//
