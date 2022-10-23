742176bbd82995e222c76733be7e7580cf7af4c7
chloeakazhixian97 <84550316+chloeakazhixian97@users.noreply.github.com>
2022-10-23 20:14:31 +0900

    push algorithm gogo

---
+++ src/test/java/codingTest/algorithm/CircularPrimeNumberTest.java
@@ -0,0 +1,29 @@
+package codingTest.algorithm;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class CircularPrimeNumberTest {
+  private char cout;
+
+  @Test
+  void test1() {
+//    printDecimal(3,8);
+    log.info("result : {} ",recursiveSum(10));
+  }
+
+  private void printDecimal(int a, int b) {
+    int iter =0;
+    while(a > 0){
+      if(iter++ == 1) System.out.printf(".");
+      System.out.printf("%d", a / b);
+      a = (a % b) * 10;
+    }
+
+  }
+  private int recursiveSum(int n){
+    if(n == 1) return 1;
+    return n + recursiveSum(n-1);
+  }
+}
