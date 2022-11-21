f23035bb569b36278df990c33e06b8d794c9a020
Jss91 <choss01@naver.com>
2022-11-21 18:29:49 +0900

    push

---
+++ src/test/java/codingTest/algorithm/_189.java
@@ -0,0 +1,25 @@
+package codingTest.algorithm;
+
+import org.junit.jupiter.api.Test;
+
+public class _189 {
+    String input = "xbwwb";
+    String output = "xwbbw";
+
+    String input_2 = "xbwxwbbwb";
+    String output_2 = "xxbwwbbbw";
+
+//  1100
+//  1100
+//  0111
+//  1011
+
+//  1011
+//  0111
+//  1100
+//  1100
+    @Test
+    void test1() {
+
+    }
+}
