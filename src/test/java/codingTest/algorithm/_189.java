58d4324cfcdf0f37921d67256c883440810759c6
JSS <choss01@naver.com>
2022-11-22 22:39:51 +0900

    quade tree reverse

--- src/test/java/codingTest/algorithm/_189.java
+++ src/test/java/codingTest/algorithm/_189.java
@@ -1,7 +1,13 @@
 package codingTest.algorithm;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+@Slf4j
 public class _189 {
     String input = "xbwwb";
     String output = "xwbbw";
@@ -20,6 +26,19 @@ public class _189 {
 //  1100
     @Test
     void test1() {
-
+        String[] headArray = input.split("");
+        Iterable<String> head = Arrays.asList(headArray);
+        String reverse = reverse(head.iterator());
+        log.info("result : {}", reverse);
+    }
+    private String reverse(Iterator<String> it){
+        String head = it.next();
+        if(head.equals("b")  || head.equals("w"))
+            return head;
+        String upperLeft = reverse(it);
+        String upperRight = reverse(it);
+        String lowerLeft = reverse(it);
+        String lowerRight = reverse(it);
+        return "x" + lowerLeft + lowerRight + upperLeft + upperRight;
     }
 }
