aeeac2a84b204faf3b67c78320a23288fa93451c
Jss91 <choss01@naver.com>
2022-12-01 17:51:03 +0900

    °¡Àå ªÀº ±æ ã±â ¾˰�Áò

--- src/test/java/codingTest/programmers/GameMapShortestPath.java
+++ src/test/java/codingTest/programmers/GameMapShortestPath.java
@@ -5,111 +5,145 @@ import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class GameMapShortestPath {
-  private static int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
-  private static int answer = 9999999;
-  private static int count =0;
-  private static int x =0;
-  private static int y =0;
+//  private static int[][] maps = {
+//          {1,0,1,1,1},
+//          {1,0,1,0,1},
+//          {1,0,1,1,1},
+//          {1,1,1,0,1},
+//          {0,0,0,0,1}
+//  };
 
-  @Test
-  void solution() {
-    int x = 0;
-    int y = 0;
-    move();
-    if(answer == 9999999){
-      log.info("restult : {}", answer);
-    }else{
-      log.info("restult : {}", answer);
-    }
-  }
-  private void move(){
-//      System.out.printf("x : %d, y : %d", x, y);
-    if(x == 4 && y == 4){
-      answer = Math.min(answer, count);
-      return;
-    }
-    if(count > 24){
-      return;
-    }
-    moveUp();
-    moveRight();
-    moveDown();
-    moveLeft();
-  }
-  private void moveUp(){
-    if(up()){
-      count++;
-      move();
-      down();
-      count--;
-    }
-  }
-  private void moveRight(){
-    if(right()){
-      count++;
-      move();
-      left();
-      count--;
-    }
-  }
-  private void moveDown(){
-    if(down()){
-      count++;
-      move();
-      up();
-      count--;
+    private static int[][] maps = {
+            {1, 1, 1, 1, 0, 1},
+            {0, 0, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1}
+    };
+    private static int answer = 9999999;
+    private static int count = 1;
+    private static int x = 0;
+    private static int y = 0;
+
+    @Test
+    void solution() {
+        int x = 0;
+        int y = 0;
+        move();
+        if (answer == 9999999) {
+            log.info("restult : {}", -1);
+        } else {
+            log.info("restult : {}", answer);
+        }
     }
 
-  }
-  private void moveLeft(){
-    if(left()){
-      count++;
-      move();
-      right();
-      count--;
+    private void move() {
+        if (x == maps[0].length -1 && y == maps.length -1) {
+            answer = Math.min(answer, count);
+            return;
+        }
+        moveUp();
+        moveRight();
+        moveDown();
+        moveLeft();
     }
-  }
 
-  private boolean up(){
-    if(y-1 < 0)
-      return false;
-    if(maps[y-1][x] == 0)
-      return false;
-    y -=1;
-    return true;
+    private void moveUp() {
+        if (up()) {
+            count++;
+            move();
+            reverseUp();
+            count--;
+        }
+    }
 
-  }
-  private boolean down(){
-    if(y+1 > 4)
-      return false;
-    if(maps[y+1][x] == 0)
-      return false;
-    y +=1;
-    return true;
-  }
-  private boolean left(){
-    if(x-1 <0)
-      return false;
-    if(maps[y][x-1] == 0)
-      return false;
-    x -=1;
-    return true;
-  }
-  private boolean right(){
-    if(x+1 >4)
-      return false;
-    if(maps[y][x+1] == 0)
-      return false;
-    x +=1;
-    return true;
-  }
-//  {    x
-//      {1,0,1,1,1} y
-//      {1,0,1,1,1}
-//      {1,0,1,1,1}
-//      {1,0,1,1,1}
-//      {1,0,1,1,1}
-//  }
+    private void moveRight() {
+        if (right()) {
+            count++;
+            move();
+            reverseRight();
+            count--;
+        }
+    }
 
-//  private static int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
+    private void moveDown() {
+        if (down()) {
+            count++;
+            move();
+            reverseDown();
+            count--;
+        }
+
+    }
+
+    private void moveLeft() {
+        if (left()) {
+            count++;
+            move();
+            reverseLeft();
+            count--;
+        }
+    }
+
+    private boolean up() {
+        if (y - 1 < 0)
+            return false;
+        if (maps[y - 1][x] == 0)
+            return false;
+        maps[y][x] = 0;
+        y -= 1;
+        return true;
+    }
+
+    private boolean reverseUp() {
+        y += 1;
+        maps[y][x] = 1;
+        return true;
+    }
+
+    private boolean down() {
+        if (y + 1 > maps.length -1)
+            return false;
+        if (maps[y + 1][x] == 0)
+            return false;
+        maps[y][x] = 0;
+        y += 1;
+        return true;
+    }
+
+    private boolean reverseDown() {
+        y -= 1;
+        maps[y][x] = 1;
+        return true;
+    }
+
+    private boolean left() {
+        if (x - 1 < 0)
+            return false;
+        if (maps[y][x - 1] == 0)
+            return false;
+        maps[y][x] = 0;
+        x -= 1;
+        return true;
+    }
+    private boolean reverseLeft() {
+        x += 1;
+        maps[y][x] = 1;
+        return true;
+    }
+
+    private boolean right() {
+        if (x + 1 > maps[0].length -1)
+            return false;
+        if (maps[y][x + 1] == 0)
+            return false;
+        maps[y][x] = 0;
+        x += 1;
+        return true;
+    }
+    private boolean reverseRight() {
+        x -= 1;
+        maps[y][x] = 1;
+        return true;
+    }
 }
