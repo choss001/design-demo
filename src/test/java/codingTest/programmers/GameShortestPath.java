0d2d01f8cc797566cfffcd45c280a4fd539c1c35
Jss91 <choss01@naver.com>
2022-12-01 12:08:06 +0900

    °¡Àå ªÀº ±æ ã±â

---
+++ src/test/java/codingTest/programmers/GameShortestPath.java
@@ -0,0 +1,18 @@
+package codingTest.programmers;
+
+import org.junit.jupiter.api.Test;
+
+public class GameShortestPath {
+
+    //11
+    int[][] input = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
+    //-1
+    int[][] input_1 = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,0},{0,0,0,0,1}};
+
+    @Test
+    void test1(){
+
+
+    }
+
+}
