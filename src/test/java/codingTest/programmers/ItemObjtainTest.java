48e5b919bcc72831efd0948e42a53d5076dc65f6
Jss91 <choss01@naver.com>
2022-12-14 16:48:22 +0900

    item obtain algorithm

--- src/test/java/codingTest/programmers/ItemObjtainTest.java
+++ src/test/java/codingTest/programmers/ItemObjtainTest.java
@@ -1,50 +1,76 @@
 package codingTest.programmers;
 
 public class ItemObjtainTest {
-  private int[] y = {1, 0, -1, 0};
-  private int[] x = {0, 1, 0, -1};
-  private static int[][] allMap = new int[50][50];
-  public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-    int answer = 0;
-    makeMap(rectangle);
-//      for(int i = 0; i< allMap.length; i++){
-//          for(int j =0; j < allMap[0].length; j ++){
-//              System.out.print(allMap[i][j]);
-//          }
-//          System.out.println();
-//
-//      }
+    private int[] y = {1, 0, -1, 0};
+    private int[] x = {0, 1, 0, -1};
+    private static int[][] allMap = new int[50][50];
+    private int answer = Integer.MAX_VALUE;
+    private int itemX;
+    private int itemY;
+
+    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        this.itemX = itemX;
+        this.itemY = itemY;
+
+        makeMap(rectangle);
+        colorMap(rectangle);
+        allMap[characterY][characterX] = 9;
+//      move(characterX, characterY);
+        for (int i = 49; i >= 0; i--) {
+            for (int j = 0; j < 50; j++) {
+                System.out.print(allMap[i][j]);
+            }
+            System.out.println();
+
+        }
 
 
-    return answer;
-  }
-  private void move(int characterX, int characterY){
-    for(int i =0; i < 4; i++){
-
+        return answer;
     }
-  }
-  private void makeMap(int[][] rectangle){
-    for(int i =0; i < rectangle.length; i++){
-      //아래
-      for(int j = rectangle[i][0]; j <= rectangle[i][2]; j++)
-        allMap[rectangle[i][1]][j] = 1;
-      //왼쪽
-      for(int j = rectangle[i][1]; j <= rectangle[i][3]; j++)
-        allMap[j][rectangle[i][0]] = 1;
-      //오른쪽
-      for(int j = rectangle[i][1]; j <= rectangle[i][3]; j++)
-        allMap[j][rectangle[i][2]] = 1;
-      //위
-      for(int j = rectangle[i][0]; j <= rectangle[i][2]; j++)
-        allMap[rectangle[i][3]][j] = 1;
-    }
-  }
-  private void colorMap(int[][] rectangle){
 
-    for(int i =0; i < rectangle.length; i++){
-      for(int j = 0; j< rectangle[i][0] - rectangle[i][2]; j++){
-        allMap[][] =
-      }
+    private void move(int characterX, int characterY) {
+        if (characterX == itemX && characterY == itemY) {
+            answer = Math.min(allMap[characterY][characterX] - 1, answer);
+            return;
+        }
+
+        int dx = characterX;
+        int dy = characterY;
+        for (int i = 0; i < 4; i++) {
+            if (allMap[dy + y[i]][dx + x[i]] == 1) {
+                dy += y[i];
+                dx += x[i];
+                allMap[dy][dx] += 5;
+            }
+        }
+        move(dx, dy);
     }
-  }
+
+    private void makeMap(int[][] rectangle) {
+        for (int i = 0; i < rectangle.length; i++) {
+            //아래
+            for (int j = rectangle[i][0]; j <= rectangle[i][2]; j++)
+                allMap[rectangle[i][1]][j] = 1;
+            //왼쪽
+            for (int j = rectangle[i][1]; j <= rectangle[i][3]; j++)
+                allMap[j][rectangle[i][0]] = 1;
+            //오른쪽
+            for (int j = rectangle[i][1]; j <= rectangle[i][3]; j++)
+                allMap[j][rectangle[i][2]] = 1;
+            //위
+            for (int j = rectangle[i][0]; j <= rectangle[i][2]; j++)
+                allMap[rectangle[i][3]][j] = 1;
+        }
+    }
+
+    private void colorMap(int[][] rectangle) {
+        for (int i = 0; i < rectangle.length; i++) {
+            for (int j = 0; j < rectangle[i][2] - rectangle[i][0] - 1; j++) {
+                for (int l = 0; l < rectangle[i][3] - rectangle[i][1] - 1; l++) {
+                    allMap[rectangle[i][1] + 1 + l][rectangle[i][0] + 1 + j] = 2;
+
+                }
+            }
+        }
+    }
 }
