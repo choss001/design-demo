73d067f31fa2c3eb5bb126d72a2d4a2463775e65
Jss91 <choss01@naver.com>
2022-12-22 10:50:12 +0900

    wild card algorithm : i am study memoization in my code

--- src/test/java/codingTest/algorithm/_222WildCardMyPracticeTest.java
+++ src/test/java/codingTest/algorithm/_222WildCardMyPracticeTest.java
@@ -6,20 +6,20 @@ import org.junit.jupiter.api.Test;
 @Slf4j
 public class _222WildCardMyPracticeTest {
 
-//    private static final String W = "a*b*c*e*b";
-    private static final String W = "a*b*ce*x";
-    private static final String S = "abbccedssb";
+    //    private static final String W = "a*b*c*e*b";
+    private static final String W = "a*b*a";
+    private static final String S = "abbccs";
     private int[][] cache = new int[101][101];
     private static int count = 0;
 
     @Test
     void test() {
-        boolean match = match(W, S);
-        log.info("result : {}", match);
+        boolean matchV1 = match(W, S);
+        log.info("result : {}", matchV1);
         log.info("match method count : {}", count);
     }
 
-    private boolean match(String w, String s) {
+    private boolean matchV1(String w, String s) {
         count++;
         makeCache();
         int pos = 0;
@@ -27,30 +27,55 @@ public class _222WildCardMyPracticeTest {
                 && (w.charAt(pos) == '?' || w.charAt(pos) == s.charAt(pos))) {
             ++pos;
         }
-        if(pos == w.length())
+        if (cache[pos][pos] != -1)
+            return cache[pos][pos] == 1 ? true : false;
+        if (pos == w.length())
             return true;
 
         //
         if (w.charAt(pos) == '*') {
-            if(pos == w.length() -1)
+            if (pos == w.length() - 1)
                 return true;
             for (int i = 1; pos + i < s.length(); i++) {
-                if(cache[pos+1][pos+i] == -1){
-                    if (match(w.substring(pos + 1), s.substring(pos + i))){
-                        cache[pos+1][pos+i] = 1;
+                if (cache[pos + 1][pos + i] == -1) {
+                    if (match(w.substring(pos + 1), s.substring(pos + i))) {
+                        cache[pos + 1][pos + i] = 1;
                         return true;
                     }
-                }else
-                    return cache[pos+1][pos+i] == 1 ? true : false;
+                } else
+                    return cache[pos + 1][pos + i] == 1 ? true : false;
             }
         }
         //3 *bc pos =0
         cache[pos][pos] = -1;
         return false;
     }
-    private void makeCache(){
-        for(int i = 0; i < cache.length; i++)
-            for(int j =0; j< cache.length; j++)
+
+    private boolean match(String w, String s) {
+        count++;
+        int pos = 0;
+        while (pos < w.length() && pos < s.length()
+                && (w.charAt(pos) == '?' || w.charAt(pos) == s.charAt(pos))) {
+            ++pos;
+        }
+        if (pos == w.length())
+            return true;
+
+        //
+        if (w.charAt(pos) == '*') {
+            if (pos == w.length() - 1)
+                return true;
+            for (int i = 1; pos + i < s.length(); i++) {
+                    if (match(w.substring(pos + 1), s.substring(pos + i)))
+                        return true;
+            }
+        }
+        return false;
+    }
+
+    private void makeCache() {
+        for (int i = 0; i < cache.length; i++)
+            for (int j = 0; j < cache.length; j++)
                 cache[i][j] = -1;
     }
 
