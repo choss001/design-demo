3731338a04f251a7d9d35f48126a67009f480a49
Jss91 <choss01@naver.com>
2022-12-25 19:55:03 +0900

    i make travelpathTEst

--- src/test/java/codingTest/programmers/TravelPathTest.java
+++ src/test/java/codingTest/programmers/TravelPathTest.java
@@ -15,25 +15,61 @@ public class TravelPathTest {
 
     private static List<String> answerList = new ArrayList<>();
 
+
+    private static List<String> answerList = new ArrayList<>();
+
     public String[] solution(String[][] tickets) {
         String[] answer = {};
         System.out.println("test : " + firstIsBigger("BAA", "ABB"));
+        Queue<List<List<List<String>>>> queue = initialList(tickets);
+        List<List<List<String>>> list = queue.poll();
+        System.out.println("result : "+list.get(0).get(0).get(0) );
+        System.out.println("result : "+list.get(0).get(0).get(1) );
+        System.out.println("result : "+list.get(0).size() );
+        System.out.println("result : "+list.get(0).get(0).size() );
+
         return answer;
     }
 
-    private void recursive(String[][] tickets) {
-        for (int i = 0; i < tickets.length; i++) {
+    private void recursive(String[][] tickets){
+        for(int i =0; i < tickets.length; i++){
 
         }
 
     }
 
     //모든 알파벳은 같을수 없다는 가정
-    private boolean firstIsBigger(String f, String s) {
-        for (int i = 0; i < 3; i++) {
-            if (f.charAt(i) < s.charAt(i))
+    private boolean firstIsBigger(String f, String s){
+        for(int i =0; i < 3; i++){
+            if(f.charAt(i) < s.charAt(i))
                 return true;
         }
         return false;
+    }
+    private Queue<List<List<List<String>>>> initialList(String[][] tickets){
+        Queue<List<List<List<String>>>> queue = new LinkedList<>();
+        for(int i =0; i < tickets.length; i++){
+            List<List<List<String>>> list = new ArrayList<>();
+            if(tickets[i][0].equals("ICN")){
+                List<List<String>> second = new ArrayList<>();
+                List<String> first = new ArrayList<>();
+                first.add(tickets[i][0]);
+                first.add(tickets[i][1]);
+                second.add(first);
+                list.add(second);
+                for(int j =0; j < tickets.length; j++){
+                    List<List<String>> secondOther = new ArrayList<>();
+                    if(j != i){
+                        List<String> firstOther = new ArrayList<>();
+                        firstOther.add(tickets[j][0]);
+                        firstOther.add(tickets[j][0]);
+                        secondOther.add(firstOther);
+                    }
+                    list.add(secondOther);
+                }
+            }
+            queue.add(list);
+        }
+        return queue;
     }
 }
