06a9d6127719bdb76a0201bc62132969910bbd7e
Jss91 <choss01@naver.com>
2022-12-29 10:30:41 +0900

    jong man book algorithm triangle result most big elemented that added path in eletement

--- src/test/java/codingTest/algorithm/_226.java
+++ src/test/java/codingTest/algorithm/_226.java
@@ -3,6 +3,9 @@ package codingTest.algorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 @Slf4j
 public class _226 {
 
@@ -13,39 +16,50 @@ public class _226 {
             {9, 4, 1, 7, 0},
             {2, 7, 5, 9, 4}
     };
-    private int y = 4;
+    private int y = 0;
     private int x = 0;
     private int answer = 0;
-    private int[] dy = {-1, 0};
+    private int[] dy = {1, 0};
     private int[] dx = {0, 1};
 
     @Test
     void test1() {
         solution(triangle, x, y);
+        Collections.sort();
+        log.info("last answer!!!! : {}", answer);
     }
 
     private int solution(int[][] triangle, int x, int y) {
-        recursive(0, 4, 6);
-        log.info("result : {}", answer);
+        int[][] move = new int[5][5];
+        move[0][0] = 1;
+        recursive(0, 0, 6, move);
+
         return 0;
     }
 
-    private void recursive(int x, int y, int answer) {
-        if (y == 0) {
+    private void recursive(int x, int y, int answer, int[][] move) {
+
+        if (y == triangle.length -1) {
             this.answer = Math.max(answer, this.answer);
+            for(int i =0; i < 5; i++){
+                for(int j =0; j < 5; j ++){
+                    System.out.print(move[i][j]);
+                }
+                System.out.println();
+            }
+            System.out.println("result : "+ answer);
+            return;
         }
 
         for (int i = 0; i < 2; i++) {
             int afterX = x + dx[i];
             int afterY = y + dy[i];
-
-            if(afterX > triangle[0].length -1 )
+            if(afterX > triangle[0].length -1 || afterY > triangle.length -1)
                 continue;
 
-            if(afterY < 0)
-                continue;
-
-            recursive(afterX, afterY, answer + triangle[afterY][afterX]);
+            move[afterY][afterX] = 1;
+            recursive(afterX, afterY, answer + triangle[afterY][afterX], move);
+            move[afterY][afterX] = 0;
         }
     }
 
