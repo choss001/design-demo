5cfd29a454e20063ccd2663ed51bbaee8fd20d70
Jss91 <choss01@naver.com>
2023-01-02 10:15:07 +0900

    programmars travel path complete!!!

--- src/test/java/codingTest/programmers/TravelPathtest_4.java
+++ src/test/java/codingTest/programmers/TravelPathtest_4.java
@@ -1,27 +1,56 @@
 package codingTest.programmers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Slf4j
 public class TravelPathtest_4 {
 
-    private String answerStr = "";
-    private boolean[] used;
-    private String[][] tickets;
+    private static final String[][] ticketsInput = new String[][]{
+            {"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}
+    };
+    private static final String[] resultValue = new String[]{
+            "ICN", "JFK", "HND", "IAD"
+    };
 
     @Test
-    void test(){
-
+    void test() {
+        String[] solution = solution(ticketsInput);
+        Assertions.assertArrayEquals(solution, resultValue);
+        Stream.of(resultValue).forEach(i -> System.out.printf("%s ", i));
     }
+
+    private List<String> answerStrList = new ArrayList<>();
+    private boolean[] used;
+    private String[][] tickets;
 
     public String[] solution(String[][] tickets) {
         used = new boolean[tickets.length];
         this.tickets = tickets;
-        String[] answer = {};
-        recursive();
-        return answer;
+
+        recursive("ICN", "ICN", 0);
+
+        Collections.sort(answerStrList);
+        return answerStrList.get(0).split(" ");
     }
-    private void recursive(){
-        for(int i =0; i < tickets.length; i++){
+
+    private void recursive(String start, String mergeStr, int count) {
+        if (count == tickets.length) {
+            this.answerStrList.add(mergeStr);
+            return;
+        }
+        for (int i = 0; i < tickets.length; i++) {
+            if (tickets[i][0].equals(start) && used[i] == false) {
+                used[i] = true;
+                recursive(tickets[i][1], mergeStr + " " + tickets[i][1], count + 1);
+                used[i] = false;
+            }
         }
     }
 }
