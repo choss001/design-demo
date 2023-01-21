a2be5322df7f34b48f9c6c128fd564ce34a803bc
Jss91 <choss01@naver.com>
2023-01-21 20:51:02 +0900

    PyramidTest process

--- src/test/java/codingTest/algorithm/PyramidTest.java
+++ src/test/java/codingTest/algorithm/PyramidTest.java
@@ -1,2 +1,23 @@
-package codingTest.algorithm;public class PyramidTest {
+package codingTest.algorithm;
+
+import org.junit.jupiter.api.Test;
+
+public class PyramidTest {
+    @Test
+    void test1(){
+        int rows = 5, k = 0;
+
+        for (int i = 1; i <= rows; ++i, k = 0) {
+            for (int space = 1; space <= rows - i; ++space) {
+                System.out.print("  ");
+            }
+
+            while (k != 2 * i - 1) {
+                System.out.print("* ");
+                ++k;
+            }
+
+            System.out.println();
+        }
+    }
 }
