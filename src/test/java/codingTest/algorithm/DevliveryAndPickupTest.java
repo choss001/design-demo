1853c484a20cd4837611e7d2659cbd4a5fd47b8b
chloeakazhixian97 <choss01@naver.com>
2023-02-06 22:40:12 +0900

    /annotation

--- src/test/java/codingTest/algorithm/DevliveryAndPickupTest.java
+++ src/test/java/codingTest/algorithm/DevliveryAndPickupTest.java
@@ -3,6 +3,8 @@ package codingTest.algorithm;
 public class DevliveryAndPickupTest {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = -1;
+
+        //주석
         return answer;
     }
 }
