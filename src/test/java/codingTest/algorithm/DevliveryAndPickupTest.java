3d6fcfc25c1d9ec2914a2516c9563007d2319e75
chloeakazhixian97 <choss01@naver.com>
2023-02-08 23:11:24 +0900

    Solving programmers practice : delivery and pickup

--- src/test/java/codingTest/algorithm/DevliveryAndPickupTest.java
+++ src/test/java/codingTest/algorithm/DevliveryAndPickupTest.java
@@ -10,8 +10,6 @@ public class DevliveryAndPickupTest {
         this.pickups = pickups;
 
         long answer = -1;
-
-        //주석
         return answer;
     }
     private void goStart(){
@@ -27,27 +25,27 @@ public class DevliveryAndPickupTest {
                     goLength = i+1;
                     break;
                 }
-
             }
-
         }
     }
-//  private int calLength(int[] targetArray){
-//      for(int i =0; i < deliveries.length; i++){
-//          int delTemp = targetArray[i];
-//          targetArray[i] = calMinus(delTemp, deliver);
-//          deliver = calMinus(deliver, delTemp);
-//          if(deliver ==0){
-//              goLength = i+1;
-//              break;
-//          }
-//      }
-//  }
+    private int calLength(int[] targetArray){
+        boolean end = false;
+        int deliver = cap;
+        int goLength = 0;
+        for(int i =0; i < targetArray.length; i++){
+            int delTemp = targetArray[i];
+            targetArray[i] = calMinus(delTemp, deliver);
+            deliver = calMinus(deliver, delTemp);
+            if(deliver ==0){
+                goLength = i+1;
+                break;
+            }
+        }
+    }
 
     private int calMinus(int target, int component){
         if(target > component)
             return target-component;
         else
             return 0;
-    }
-}
+    }}
