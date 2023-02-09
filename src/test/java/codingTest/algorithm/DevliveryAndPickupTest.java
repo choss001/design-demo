b32bc89f42c10833f68e072777c750caa476d523
chloeakazhixian97 <choss01@naver.com>
2023-02-09 20:39:36 +0900

    Solving programmer practise : deliver

--- src/test/java/codingTest/algorithm/DevliveryAndPickupTest.java
+++ src/test/java/codingTest/algorithm/DevliveryAndPickupTest.java
@@ -4,48 +4,52 @@ public class DevliveryAndPickupTest {
     private int cap;
     private int[] deliveries;
     private int[] pickups;
+    private long answer;
+
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         this.cap = cap;
         this.deliveries = deliveries;
         this.pickups = pickups;
 
-        long answer = -1;
+        goStart();
         return answer;
     }
-    private void goStart(){
-        boolean end = false;
-        int deliver = cap;
-        int goLength = 0;
-        while(end){
-            for(int i =0; i < deliveries.length; i++){
-                int delTemp = deliveries[i];
-                deliveries[i] = calMinus(delTemp, deliver);
-                deliver = calMinus(deliver, delTemp);
-                if(deliver ==0){
-                    goLength = i+1;
-                    break;
-                }
-            }
-        }
-    }
-    private int calLength(int[] targetArray){
-        boolean end = false;
-        int deliver = cap;
-        int goLength = 0;
-        for(int i =0; i < targetArray.length; i++){
-            int delTemp = targetArray[i];
-            targetArray[i] = calMinus(delTemp, deliver);
-            deliver = calMinus(deliver, delTemp);
-            if(deliver ==0){
-                goLength = i+1;
-                break;
-            }
+
+    private void goStart() {
+        boolean end = true;
+        while (end) {
+            int deliverLength = calLength(deliveries);
+            int pickupLength = calLength(pickups);
+            answer += Math.max(deliverLength, pickupLength) * 2;
+            end = isNotFinish();
         }
     }
 
-    private int calMinus(int target, int component){
-        if(target > component)
-            return target-component;
+    private boolean isNotFinish() {
+        for (int i = 0; i < deliveries.length; i++) {
+            if (0 < deliveries[i] || 0 < pickups[i])
+                return true;
+        }
+        return false;
+    }
+
+    private int calLength(int[] targetArray) {
+        int tempCap = cap;
+        for (int i = 0; i < targetArray.length; i++) {
+            int delTemp = targetArray[i];
+            targetArray[i] = calMinus(delTemp, tempCap);
+            tempCap = calMinus(tempCap, delTemp);
+            if (tempCap == 0)
+                return i + 1;
+        }
+        System.out.println("why : " + tempCap);
+        return 9999999;
+    }
+
+    private int calMinus(int target, int component) {
+        if (target > component)
+            return target - component;
         else
             return 0;
-    }}
+    }
+}
