8c7a33ad8b239e6fda28a9bfcc72d6befa3b3096
choss001 <choss01@naver.com>
2023-02-21 21:40:35 +0900

    backjoon algorithm pickture

---
+++ src/test/java/codingTest/backjoon/_1926.java
@@ -0,0 +1,21 @@
+package codingTest.backjoon;
+
+import org.junit.jupiter.api.Test;
+
+public class _1926 {
+
+//    6 5
+//            1 1 0 1 1
+//            0 1 1 0 0
+//            0 0 0 0 0
+//            1 0 1 1 1
+//            0 0 1 1 1
+//            0 0 1 1 1
+
+    @Test
+    void test1(){
+
+    }
+
+    private
+}
