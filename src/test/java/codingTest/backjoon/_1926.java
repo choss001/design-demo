6435a82da848ffa4b4682239783ac2c9f5c2a91a
choss001 <choss01@naver.com>
2023-02-22 23:18:49 +0900

    Solving backjoon algorithm retangle

--- src/test/java/codingTest/backjoon/_1926.java
+++ src/test/java/codingTest/backjoon/_1926.java
@@ -11,11 +11,38 @@ public class _1926 {
 //            1 0 1 1 1
 //            0 0 1 1 1
 //            0 0 1 1 1
+    int[][] input = {
+        {1, 1, 0, 1, 1},
+        {0, 1, 1, 0, 0},
+        {0, 0, 0, 0, 0},
+        {1, 0, 1, 1, 1},
+        {0, 0, 1, 1, 1},
+        {0, 0, 1, 1, 1}
+    };
+    boolean[][] cache = new boolean[6][5];
+
+    int[] x = {1, 0, -1, 0};
+    int[] y = {0, -1, 0, 1};
 
     @Test
     void test1(){
 
     }
 
-    private
+    private void recursive(){
+        int curX = 0;
+        int curY = 0;
+        for(int i = 0; i < 4; i++){
+            curX = x[i];
+            curY = y[i];
+        }
+
+        for(int i =0; i< input.length; i++ ){
+            for (int j = 0; j < input[0].length; j++) {
+                
+
+            }
+        }
+    }
+
 }
