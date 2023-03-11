960db01b6894d05ebc4b0ee6ae3e3dab364fe2b8
JSS <choss01@naver.com>
2023-03-11 22:44:06 +0900

    thread practice

---
+++ src/main/java/com/example/demo/thread/Thread.java
@@ -0,0 +1,26 @@
+package com.example.demo.thread;
+
+    public class Thread extends Thread {
+
+        public Thread(String name) {
+            super(name);
+        }
+
+        @Override
+        public void run() {
+            System.out.println("MyThread - START "+Thread.currentThread().getName());
+            try {
+                Thread.sleep(1000);
+                //Get database connection, delete unused data from DB
+                doDBProcessing();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("MyThread - END "+Thread.currentThread().getName());
+        }
+
+        private void doDBProcessing() throws InterruptedException {
+            Thread.sleep(5000);
+        }
+
+    }
